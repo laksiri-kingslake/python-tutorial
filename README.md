65972d6cbb3e72371f7fdb3ca7de89019ed394e6
Laksiri Fernando <laksirif@kingslake.com>
2025-03-03 11:56:29 +0530

    data structures

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ python3 -m venv myenv
 source myenv/bin/activate
 ```
 
+## VS Code shortcut keys
+1. Multi line comment: select lines [Shift+Alt+A]
+2. Single line comment: select lines [Ctrl+/]
---
+++ data-structures.py
@@ -0,0 +1,19 @@
+letters = ['a','b','c','d']
+matrix=[[0,1],[2,3]]
+zeros=[0]*5
+combined=zeros+letters
+numbers=list(range(20)) # iterable
+chars=list("Hello World")  #string is a iterable
+
+# print(chars)
+# print(numbers)
+# print(combined)
+# print(matrix)
+# print(letters[0])
+# print(letters[-1])
+# print(letters[0:3])
+# print(letters[:3])
+
+letters[0] = "A"
+print(letters)
+
